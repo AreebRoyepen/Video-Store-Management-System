2e4f339b82090751139250ed2a8b7c63dd024e0c
Areeb <areeb.royepen@gmail.com>
2019-08-13 22:18:52 +0200

    Some tables added

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Media, Payment, Booked
 
 # Register your models here.
+
+admin.site.register(Media)
+admin.site.register(Payment)
+admin.site.register(Booked)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Media(models.Model):
+
+	ID = models.CharField(max_length = 120, primary_key=True)
+	mtype = models.CharField(max_length = 120)
+	primTitle = models.CharField(max_length = 120)
+	originalTitle = models.CharField(max_length = 120)
+	isAdult = models.BooleanField()
+	startyear = models.DecimalField(max_digits =10, decimal_places=0)
+	endyear = models.DecimalField(max_digits =10, decimal_places=0)
+	runtime = models.DecimalField(max_digits =10, decimal_places=0	)
+	genres = models.CharField(max_length = 120)
+	isBooked = models.BooleanField()
+	price = models.DecimalField(decimal_places = 2 , max_digits=10000)
+	poster = models.CharField(max_length = 120)
+	description = models.TextField(max_length = 120)
+
+class Payment(models.Model):
+	username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+	ID = models.ForeignKey(Media, on_delete=models.CASCADE, related_name='+')
+	price = models.ForeignKey(Media, on_delete=models.CASCADE, related_name='+')
+
+class Booked(models.Model):
+	ID = models.ForeignKey(Media, on_delete=models.CASCADE, related_name='+')
+	username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
+	#returnBy = getDateForWhenToReturnTo()
